6bbd958f1fbc146da82a9576716be2b63a59a7a8
Andrei Miculita <andreimiculita@gmail.com>
2017-11-14 20:53:35 +0200

    Changed database structure to support order system

--- pizzasite/pizza/admin.py
+++ pizzasite/pizza/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Variety
 
-admin.site.register(Question)
+admin.site.register(Variety)
--- pizzasite/pizza/models.py
+++ pizzasite/pizza/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 from django.template.defaultfilters import slugify
 
 
-class Question(models.Model):
+class Variety(models.Model):
     question_text = models.CharField(max_length=200)
     question_desc = models.CharField(max_length=200)
     link_str = models.SlugField(max_length=200, blank=True)
@@ -19,12 +19,19 @@ class Question(models.Model):
     def save(self, *args, **kwargs):
         self.link_str = slugify(self.question_text)
 
-        super(Question, self).save(*args, **kwargs)
+        super(Variety, self).save(*args, **kwargs)
+
+
+class Order(models.Model):
+    address = models.CharField(max_length=200)
+    time_placed = models.DateTimeField('date order was placed')
+    products = models.ManyToManyField(Variety, through="Choice")
+
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    variety = models.ForeignKey(Variety, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
     amount = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choice_text
+        return self.variety.__str__() + self.amount.__str__()
